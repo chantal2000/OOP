0b10b7575dae26976836a2b2740d08600220441f
chantal2000 <nniyonkuruchantal@gmail.com>
2021-03-23 21:39:50 +0300

    Using class in OOP

---
+++ src/OOP Assignment.kt
@@ -0,0 +1,27 @@
+fun main(){
+    var person=Human("Chantal",30,50)
+    person.eat(30)
+    person.speak("Love")
+    person.birthday()
+    var client=User("Niyonkuru","Marie","nniyonkuruchantal@gmail.com","+250786738069","Chantos2")
+    println(client.password)
+    println(client.lastName)
+
+}
+class Human(var name:String,var age:Int,var weight:Int){
+    fun eat(foodWeight:Int){
+        println("I am eating {$foodWeight}Kgs of Food")
+        weight=weight+foodWeight
+        println(weight)
+    }
+    fun speak(speech:String){
+        println("Love")
+    }
+    fun birthday(){
+        age=age+1
+        println(age)
+    }
+}
+data class User(var firstName:String,var lastName:String,var email:String,var phoneNumber:String,var password:String){
+
+}
